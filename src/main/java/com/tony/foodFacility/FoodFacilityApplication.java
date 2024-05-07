a2598d88e7abf34fc9d8535a5dae44fc9f38f43d
cool_sweet <cool_sweet@sina.com>
2024-05-07 16:28:17 +0800

    init

---
+++ src/main/java/com/tony/foodFacility/FoodFacilityApplication.java
@@ -0,0 +1,13 @@
+package com.tony.foodFacility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFacilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFacilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tony/foodFacility/FoodFacilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.foodFacility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFacilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
